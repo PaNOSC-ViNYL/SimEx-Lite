83d04bca4cb8bef6fb64cf40fe73c5c75b6d8aee
Juncheng E <juncheng.e@xfel.eu>
2021-10-20 16:03:54 +0200

    Bump version: 0.3.1 → 0.3.2

--- setup.py
+++ setup.py
@@ -52,6 +52,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/PaNOSC-ViNYL/SimEx-Lite',
-    version='0.3.1',
+    version='0.3.2',
     zip_safe=False,
 )
