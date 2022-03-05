e7fbbfd23d5c7c9cc1808d433db427254df21e11
Juncheng E <juncheng.e@xfel.eu>
2022-03-05 17:32:30 +0100

    Add SampleData and ASEFormat

--- tests/test_SampleData.py
+++ tests/test_SampleData.py
@@ -1,17 +1,49 @@
 """Test MolecularDynamicsData"""
 
-from SimExLite.SampleData import MolecularDynamicsData
 import h5py
+import numpy as np
+import pytest
+from SimExLite.SampleData import SampleData, ASEFormat
 
 
-def test_XMDYNData():
-    MDD = MolecularDynamicsData("./testFiles/xmdyn.h5")
-    assert MDD.file_format == "XMDYN"
+@pytest.fixture(scope="session")
+def tmp_data():
+    data_dict = {}
+    # 2 atoms
+    positions = np.empty((2, 3))
+    positions[0] = np.array([8, 18, -3.54])
+    positions[1] = np.array([9, 18.2, -3.1])
+    data_dict["positions"] = positions
+    data_dict["atomic_numbers"] = np.array([8, 6])
+    return data_dict
 
 
-def test_UnknownData(tmp_path):
-    tmp_h5 = str(tmp_path / 'tmp.h5')
-    with h5py.File(tmp_h5, 'w') as h5:
-        h5.create_group('test_g')
-    MDD = MolecularDynamicsData(tmp_h5)
-    assert MDD.file_format == "UNKNOWN"
+def test_create_from_dict(tmp_data):
+    """Test creating a SampleData from data_dict"""
+    SD = SampleData.from_dict(tmp_data, key="tmp_data")
+    data_dict = SD.get_data()
+    print(data_dict)
+    print(tmp_data)
+    # assert False
+    assert data_dict == tmp_data
+
+
+@pytest.fixture(scope="session")
+def SD_pdb(tmp_data, tmp_path_factory):
+    """Test writing a pdb file from"""
+    SD = SampleData.from_dict(tmp_data, key="tmp_data")
+    fn = tmp_path_factory.mktemp("data") / "atoms.pdb"
+    SD_pdb = SD.write(str(fn), ASEFormat)
+    return SD_pdb
+
+
+def test_create_from_file(SD_pdb, tmp_data):
+    """Test creating a SampleData from data_dict"""
+    filename = SD_pdb.filename
+    SD = SampleData.from_file(filename, ASEFormat, key="tmp_data")
+    data_dict = SD.get_data()
+    assert SD.mapping_type == ASEFormat
+    print(data_dict)
+    print(tmp_data)
+    assert np.all(data_dict["positions"] == tmp_data["positions"])
+    assert np.all(data_dict["atomic_numbers"] == tmp_data["atomic_numbers"])
