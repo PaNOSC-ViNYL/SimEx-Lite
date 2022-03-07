7530e7d63666d581ba9381ce4885a06f5436a371
Nagy Gergely <nagygeri11@gmail.com>
2022-03-07 14:37:30 +0100

    Removed WPG_path parameter; moved import wpg to the beginning as optional dependency

--- tests/test_GaussianSourceCalculator.py
+++ tests/test_GaussianSourceCalculator.py
@@ -2,12 +2,11 @@
 
 import pytest
 from SimExLite.SourceCalculators import GaussianSourceCalculator
-
-WPG_path = "/home/juncheng/GPFS/exfel/data/user/juncheng/WPG"
+from SimExLite.WavefrontData import WavefrontData
 
 
 def test_run_default_parameters():
-    calculator = GaussianSourceCalculator("gaussian_source", WPG_path)
+    calculator = GaussianSourceCalculator("gaussian_source")
     print(calculator.parameters)
     data_out = calculator.backengine()
     print(type(data_out))
@@ -21,6 +20,5 @@ def test_run_default_parameters():
 
 
 def test_wrong_WPG_path(tmpdir):
-    WPG_path_wrong = str(tmpdir / "xx")
     with pytest.raises(ValueError):
-        GaussianSourceCalculator("gaussian_source", WPG_path_wrong)
+        GaussianSourceCalculator("gaussian_source")
