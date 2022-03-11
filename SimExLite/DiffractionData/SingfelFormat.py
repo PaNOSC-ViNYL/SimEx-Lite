51603865f079b865e5f271a1123c08bde3ca3b9c
Juncheng E <juncheng.e@xfel.eu>
2022-03-11 13:19:20 +0100

    Diffractiondata SingfelFormat ready

---
+++ SimExLite/DiffractionData/SingfelFormat.py
@@ -0,0 +1,331 @@
+import h5py
+import numpy as np
+from tqdm import tqdm
+from libpyvinyl.BaseFormat import BaseFormat
+from extra_geom import GenericGeometry
+from extra_geom.base import DetectorGeometryBase
+import SimExLite
+from SimExLite.utils.io import parseIndex
+from SimExLite.PhotonBeamData import SimpleBeam
+
+
+class SingfelFormat(BaseFormat):
+    def __init__(self) -> None:
+        super().__init__()
+
+    @classmethod
+    def format_register(self):
+        key = "singfel"
+        description = "Singfel format for DiffractionData"
+        file_extension = ".h5"
+        read_kwargs = ["index", "poissonize"]
+        write_kwargs = ["ideal_arr"]
+        return self._create_format_register(
+            key, description, file_extension, read_kwargs, write_kwargs
+        )
+
+    @staticmethod
+    def direct_convert_formats():
+        # Assume the format can be converted directly to the formats supported by these classes:
+        # AFormat, BFormat
+        # Redefine this `direct_convert_formats` for a concrete format class
+        return []
+
+    @classmethod
+    def read(cls, filename: str, index=None, poissonize=False) -> dict:
+        """Read the data from the file with the `filename` to a dictionary. If poissonize=True,
+        it will read the poissonized data, instead of the ideal one."""
+        data_dict = {}
+
+        index = parseIndex(index)
+        arr_size = len(range(getPatternTotal(filename))[index])
+        pattern_shape = getPatternShape(filename)
+        if arr_size < 1e9:
+            arr = np.zeros((arr_size, pattern_shape[0], pattern_shape[1]))
+        else:
+            arr = np.memmap(
+                "memmap.dat",
+                dtype="float16",
+                mode="w+",
+                shape=(arr_size, pattern_shape[0], pattern_shape[1]),
+            )
+        quaternions = np.zeros((arr_size, 4))
+        if isinstance(index, (slice, str)):
+            with tqdm(total=arr_size) as progress_bar:
+                for i, (pattern, quaternion) in enumerate(
+                    ireadPattern(filename, index, poissonize)
+                ):
+                    arr[i] = pattern
+                    quaternions[i] = quaternion
+                    progress_bar.update(1)  # update progress
+        else:
+            # If only reading one pattern
+            arr[0], quaternions[0] = next(ireadPattern(filename, index, poissonize))
+
+        params = getParameters(filename)
+        geom, distance, pixel_mask = params2extra_geom(params["geom"])
+        beam = params2SimpleBeam(params["beam"])
+
+        data_dict["img_array"] = arr
+        data_dict["quaternions"] = quaternions
+        data_dict["geom"] = geom
+        data_dict["distance"] = distance
+        # good_pixel = 1, bad_pixel = 0.
+        data_dict["pixel_mask"] = pixel_mask
+        data_dict["beam"] = beam
+
+        return data_dict
+
+    @classmethod
+    def write(cls, object, filename: str, arr_poisson=None, key: str = None):
+        """Save the data with the `filename`."""
+        data_dict = object.get_data()
+        method_desciption = "Written by SimEx-Lite"
+        geom = extra_geom2params(
+            data_dict["geom"], data_dict["distance"], data_dict["pixel_mask"]
+        )
+        beam = BeamData2params(data_dict["beam"])
+
+        prepH5(filename)
+        write_singfelDiffr(
+            filename,
+            arr_ideal=data_dict["img_array"],
+            geom=geom,
+            beam=beam,
+            quaternions=data_dict["quaternions"],
+            method_desciption=method_desciption,
+            arr_poisson=arr_poisson,
+        )
+
+        if key is None:
+            original_key = object.key
+            key = original_key + "_to_SingfelFormat"
+        return object.from_file(filename, cls, key)
+
+
+def ireadPattern(filename, index=None, poissonize=True):
+    """Iterator for reading diffraction patterns from a singfel file."""
+    index = parseIndex(index)
+    pattern_type = getPatternType(poissonize)
+    with h5py.File(filename, "r") as h5:
+        data_grp = h5["data"]
+        data_list = list(data_grp)
+        data_list.sort()
+        indices = data_list[index]
+
+        for i in indices:
+            yield data_grp[i][pattern_type][...], data_grp[i]["angle"][...]
+
+
+def getPatternType(poissonize: bool) -> str:
+    """Get the pattern type for reading h5 files.
+
+    Args:
+        poissonize (bool): If it's True, the function returns thegroup name
+        of the poissonized diffraction pattern, otherwise the ideal pattern.
+
+    Returns:
+        string: The group name of corresponding data.
+    """
+    if poissonize:
+        pattern_type = "data"
+    else:
+        pattern_type = "diffr"
+    return pattern_type
+
+
+def getPatternShape(filename):
+    """Get the shape of diffraction patterns in the hdf5 file"""
+    with h5py.File(filename, "r") as h5:
+        group_name = list(h5["data"])[0]
+        try:
+            return h5["data"][group_name]["diffr"].shape
+        except KeyError:
+            return h5["data"][group_name]["data"].shape
+
+
+def getPatternTotal(filename):
+    """Get the total number of diffraction patterns in the hdf5 file"""
+    with h5py.File(filename, "r") as h5:
+        npattern = len(h5["data"])
+    return npattern
+
+
+def getParameters(filename):
+    """Get a dictionary of the beam parameters and geometry parameters in a Singfel Diffr
+    HDF5 file.
+    """
+
+    # Setup return dictionary.
+    parameters_dict = {"beam": {}, "geom": {}}
+
+    # Open file.
+    with h5py.File(filename, "r") as h5:
+        # Loop over entries in /params.
+        for top_key in ["beam", "geom"]:
+            # Loop over groups.
+            for key, val in h5["params/%s" % (top_key)].items():
+                # Insert into return dictionary.
+                parameters_dict[top_key][key] = val[()]
+    # Return.
+    return parameters_dict
+
+
+def params2extra_geom(file_geom):
+    # The geometry dict from the file
+    pixel_size = file_geom["pixelHeight"]
+    mask = file_geom["mask"]
+    center_pixel = (np.array(mask.shape)) / 2
+    corner_coordinates = -center_pixel * pixel_size
+    corner_coordinates = np.append(corner_coordinates, 0)
+    simple_config = {
+        "pixel_size": pixel_size,
+        "slow_pixels": mask.shape[0],
+        "fast_pixels": mask.shape[1],
+        "corner_coordinates": [corner_coordinates],
+        "ss_vec": np.array([0, 1, 0]),
+        "fs_vec": np.array([1, 0, 0]),
+    }
+    geom = GenericGeometry.from_simple_description(**simple_config)
+    distance = file_geom["detectorDist"]
+    pixel_mask = file_geom["mask"]
+    return geom, distance, pixel_mask
+
+
+def params2SimpleBeam(file_beam):
+    focus_area = file_beam["focusArea"]
+    photon_energy = file_beam["photonEnergy"]
+    beam = SimpleBeam(photon_energy=photon_energy, focus_area=focus_area)
+    return beam
+
+
+def extra_geom2params(geom: DetectorGeometryBase, distance, pixel_mask):
+    if geom.n_modules != 1:
+        raise ValueError("pysingfel can only deal with single-module geometry.")
+    # extra_geom only deals with square pixel yet.
+    pixel_size = geom.pixel_size
+    geom_param = {
+        "detectorDist": distance,
+        "mask": pixel_mask,
+        "pixelHeight": pixel_size,
+        "pixelWidth": pixel_size,
+    }
+    return geom_param
+
+
+def BeamData2params(beam: SimpleBeam):
+    focus_area = beam.get_focus_area().magnitude
+    photon_energy = beam.get_photon_energy().magnitude
+    beam_param = {"focusArea": focus_area, "photonEnergy": photon_energy}
+    return beam_param
+
+
+def prepH5(filename):
+    """
+    Create output file, prepare top level groups, write metadata.
+    """
+    with h5py.File(filename, "w") as f:
+        # Generate top level groups
+        f.create_group("data")
+        f.create_group("params")
+        f.create_group("misc")
+        f.create_group("info")
+
+        # Write metadata
+        # Package format version
+        f.create_dataset(
+            "info/package_version", data=np.string_("SimExLite" + SimExLite.__version__)
+        )
+        # Contact
+        f.create_dataset(
+            "info/contact",
+            data=np.string_(
+                "{} <{}>".format(SimExLite.__author__, SimExLite.__email__)
+            ),
+        )
+        # Data Description
+        f.create_dataset(
+            "info/data_description",
+            data=np.string_(
+                "This dataset contains diffraction patterns written using SimEx singfelDiffr API."
+            ),
+        )
+        # Data format version
+        f.create_dataset("version", data=np.string_("0.2"))
+
+
+def write_singfelDiffr(
+    filename,
+    arr_ideal,
+    geom,
+    beam,
+    arr_poisson=None,
+    quaternions=None,
+    method_desciption="",
+    pmi_file_list=None,
+):
+    """
+    Save pattern arrays as pysingfel diffraction data.
+
+    :param filename: Output filename.
+    :type filename: string
+    :param arr_counts: The array of photon counting patterns after adding Poisson noise.
+    :type arr_counts: `np.array`
+    :param arr_intensity: The array of original diffraction patterns.
+    :type arr_intensity: `np.array`
+    :param quaternions: The list of the quaternion with which each diffraction pattern was generated.
+    :type quaternions: list-like
+    :param det: The dictionary of detector parameters.
+    :type det: dict
+    :param beam: The dictionary of beam parameters.
+    :type beam: dict
+    :param pmi_file_list: The list of the corresponding pmi output file of each diffraction pattern.
+    :type pmi_file_list: list
+    """
+    prepH5(filename)
+    # Method Description
+    with h5py.File(filename, "a") as f:
+        f.create_dataset("info/method_description", data=np.string_(method_desciption))
+        # Flush to print it before tqdm
+        print("Writing singfelDiffr data: diffr...", flush=True)
+        for i, pattern_counts in enumerate(tqdm(arr_ideal)):
+            group_name = "/data/" + "{0:07}".format(i + 1) + "/"
+            f.create_dataset(group_name + "diffr", data=pattern_counts)
+            # if pmi_file_list is not None:
+            # __write_pmi_file_list(pmi_file_list, group_name, f, i)
+
+        if arr_poisson is not None:
+            print("Writing singfelDiffr data: data...", flush=True)
+            for i, pattern_counts in enumerate(tqdm(arr_poisson)):
+                group_name = "/data/" + "{0:07}".format(i + 1) + "/"
+                f.create_dataset(group_name + "data", data=pattern_counts)
+
+        if quaternions is not None:
+            print("Writing singfelDiffr data: angle...", flush=True)
+            for i, quaternion in enumerate(tqdm(quaternions)):
+                group_name = "/data/" + "{0:07}".format(i + 1) + "/"
+                f.create_dataset(group_name + "angle", data=quaternion)
+
+        # Geometry
+        f.create_dataset("params/geom/detectorDist", data=geom["detectorDist"])
+        f.create_dataset("params/geom/pixelWidth", data=geom["pixelWidth"])
+        f.create_dataset("params/geom/pixelHeight", data=geom["pixelHeight"])
+        f.create_dataset("params/geom/mask", data=geom["mask"])
+
+        # Beam
+        f.create_dataset("params/beam/focusArea", data=beam["focusArea"])
+        f.create_dataset("params/beam/photonEnergy", data=beam["photonEnergy"])
+
+
+def __write_pmi_file_list(pmi_file_list, group_name, f, i):
+    # Link history from input pmi file into output diffr file
+    group_name_history = group_name + "history/parent/detail/"
+    group_name_history = "history/parent/detail/"
+    f[group_name + "/history/parent/parent"] = h5py.ExternalLink(
+        pmi_file_list[i], "history/parent"
+    )
+    f[group_name_history + "data"] = h5py.ExternalLink(pmi_file_list[i], "data")
+    f[group_name_history + "info"] = h5py.ExternalLink(pmi_file_list[i], "info")
+    f[group_name_history + "misc"] = h5py.ExternalLink(pmi_file_list[i], "misc")
+    f[group_name_history + "params"] = h5py.ExternalLink(pmi_file_list[i], "params")
+    f[group_name_history + "version"] = h5py.ExternalLink(pmi_file_list[i], "version")
--- tests/test_DiffractionData.py
+++ tests/test_DiffractionData.py
@@ -1,244 +1,37 @@
 """Test DiffractionData"""
 
-import matplotlib.pyplot as plt
-from SimExLite.DiffractionData.DiffractionData import DiffractionData
-import numpy as np
 import pytest
-import SimExLite.DiffractionData as DD
-from SimExLite.utils.io import UnknownFileTypeError
-from pprint import pprint
-from pathlib import Path
+from SimExLite.DiffractionData import DiffractionData, SingfelFormat
 
 
 def test_print_format_keys():
-    DD.listFormats()
-
-
-def test_filetype_content():
-    h5_file = './testFiles/singfel.h5'
-    format = DD.filetype_content(h5_file)
-    assert format == 'singfel'
-
-    md_file = './testFiles/README.md'
-    format = DD.filetype_content(md_file)
-    assert format == 'UNKNOWN'
-
-
-def test_write_emc_h5_single(tmp_path):
-    h5_file = './testFiles/singfel.h5'
-    out_path = tmp_path / "h5.emc"
-    DiffrData = DD.read(h5_file, poissonize=False)
-    DiffrData.multiply(1e4)
-    assert isinstance(DiffrData, DiffractionData) is True
-    DD.write(out_path, DiffrData, 'emc')
-
-
-def test_read_emc_h5_single():
-    h5_file = './testFiles/h5.emc'
-    DiffrData = DD.read(h5_file, pattern_shape=[81, 81])
-    DiffrData.plotPattern(0)
-    with pytest.raises(IndexError):
-        DiffrData.plotPattern(1)
-
-
-def test_write_emc_h5_multi(tmp_path):
-    h5_file = './testFiles/singfel-multi.h5'
-    out_path = tmp_path / "h5_multi.emc"
-    DiffrData = DD.read(h5_file, poissonize=False)
-    DiffrData.multiply(1e9)
-    assert isinstance(DiffrData, DiffractionData) is True
-    DD.write(out_path, DiffrData, 'emc')
-
-
-def test_read_emc_h5_multi():
-    h5_file = './testFiles/h5_multi.emc'
-    DiffrData = DD.read(h5_file, pattern_shape=[81, 81])
-    DiffrData.plotPattern(1)
-    assert len(DiffrData.array) == 13
-
-
-def test_filetype_keywords():
-    kws = {'poissonize': True}
-    format = DD.filetype_keywords(kws)
-    assert format == 'singfel'
-    kws = {'woo': True}
-    format = DD.filetype_keywords(kws)
-    assert format == 'UNKNOWN'
-
-
-def test_filetype():
-    h5_file = './testFiles/singfel.h5'
-    format = DD.filetype(h5_file)
-    assert format == 'singfel'
-    try:
-        md_file = './testFiles/README.md'
-        format = DD.filetype(md_file)
-    except UnknownFileTypeError:
-        assert True
-
-
-def test_unexpected_read_argument(tmp_path):
-    diffr_path = './testFiles/singfel-multi.h5'
-    with pytest.raises(TypeError) as excinfo:
-        DD.read(diffr_path, pattern_shape=[81, 81])
-        assert "got an unexpected keyword argument" in str(excinfo.value)
+    DiffractionData.list_formats()
 
 
 def test_read():
-    h5_file = './testFiles/singfel.h5'
-    DiffrData = DD.read(h5_file)
-    assert isinstance(DiffrData, DD.DiffractionData) is True
-
-
-def test_file_not_exists():
-    h5_file = './testFiles/xx.h5'
-    with pytest.raises(FileNotFoundError):
-        DD.read(h5_file)
+    h5_file = "./testFiles/singfel.h5"
+    DiffrData = DiffractionData.from_file(
+        h5_file, format_class=SingfelFormat, key="test_singfel", poissonize=False
+    )
+    assert isinstance(DiffrData, DiffractionData) is True
 
 
 def test_readSingFEL():
-    h5_file = './testFiles/singfel-multi.h5'
-    data = DD.read(h5_file)
-    assert len(data.array) == 13
+    h5_file = "./testFiles/singfel-multi.h5"
+    DiffrData = DiffractionData.from_file(
+        h5_file, format_class=SingfelFormat, key="test_singfel", poissonize=False
+    )
+    data_dict = DiffrData.get_data()
+    assert len(data_dict["img_array"]) == 13
 
 
 def test_writeSingFEL(tmp_path):
-    h5_file = './testFiles/singfel-multi.h5'
-    data = DD.read(h5_file)
-    ofn = tmp_path / 'rewrite_singfel.h5'
-    DD.write(str(ofn), data, 'singfel')
-
-
-def test_readRewriteSingFEL(tmp_path):
-    h5_file = './testFiles/singfel-multi.h5'
-    data = DD.read(h5_file)
-    ofn = tmp_path / 'rewrite_singfel.h5'
-    DD.write(str(ofn), data, 'singfel')
-    DD.read(str(ofn), format='singfel')
-
-
-def test_readAutoRewriteSingFEL(tmp_path):
-    h5_file = './testFiles/singfel-multi.h5'
-    data = DD.read(h5_file)
-    ofn = tmp_path / 'rewrite_singfel.h5'
-    DD.write(str(ofn), data, 'singfel')
-    DD.read(str(ofn))
-
-
-def test_listFormats():
-    DD.listFormats()
-
-
-def test_writeUnsupported():
-    input_arr = np.random.rand(10, 10)
-    data = DiffractionData(arrary=input_arr)
-    with pytest.raises(UnknownFileTypeError) as excinfo:
-        DD.write("filename", data, format='blabla')
-        print(excinfo.value)
-    assert "Unsupported format" in str(excinfo.value)
-
-
-def test_addGaussianNoise(tmp_path):
-    h5_file = './testFiles/singfel-multi.h5'
-    data = DD.read(h5_file)
-    xcs = np.array([-1.845, 56.825, 114.623])
-    fwhms = np.array([20.687, 26.771, 29.232])
-    hist_params = DD.histogramParams(xcs, fwhms)
-    data.addGaussianNoise(hist_params.mu, hist_params.sigs_popt)
-    out_path = tmp_path / "fitting.png"
-    fn = str(out_path)
-    hist_params.plotFitting(fn)
-    assert out_path.is_file() is True
-
-
-def test_addBeamStop():
-    h5_file = './testFiles/singfel-multi.h5'
-    data = DD.read(h5_file)
-    data.addBeamStop(3)
-
-
-def test_plotPattern():
-    h5_file = './testFiles/singfel-multi.h5'
-    data = DD.read(h5_file, poissonize=False)
-    data.plotPattern(idx=0, logscale=True)
-
-
-def test_savePattern(tmp_path):
-    h5_file = './testFiles/singfel-multi.h5'
-    dd = DD.read(h5_file, poissonize=False)
-    out_path = tmp_path / "test.png"
-    dd.plotPattern(idx=0, logscale=True, fn_png=str(out_path))
-    assert out_path.is_file() is True
-
-
-def test_photon_statistics():
-    h5_file = './testFiles/singfel-multi.h5'
-    dd = DD.read(h5_file, poissonize=False)
-    pprint(dd.photon_statistics)
-
-
-def test_plotStatistics():
-    h5_file = './testFiles/singfel-multi.h5'
-    dd = DD.read(h5_file, poissonize=False)
-    if __name__ == "__main__":
-        dd.plotHistogram()
-
-
-def test_saveHistogram(tmp_path):
-    h5_file = './testFiles/singfel-multi.h5'
-    dd = DD.read(h5_file, poissonize=False)
-    out_path = tmp_path / "test.png"
-    dd.plotHistogram(fn_png=str(out_path))
-    assert out_path.is_file() is True
-
-
-def test_multiply():
-    h5_file = './testFiles/singfel-multi.h5'
-    dd = DD.read(h5_file, poissonize=False)
-    orig = dd.array
-    val_orig = np.max(orig[0])
-    dd.multiply(0)
-    multi = dd.array
-    assert np.max(orig[0]) == val_orig
-    assert np.max(multi[0]) == 0
-    assert np.max(multi[2]) == 0
-
-
-def test_pattern_total():
-    h5_file = './testFiles/singfel-multi.h5'
-    dd = DD.read(h5_file)
-    assert dd.pattern_total == 13
-
-
-def test_data_simplebeam():
-    h5_file = './testFiles/singfel-multi.h5'
-    dd = DD.read(h5_file)
-    assert pytest.approx(dd.beam.attrs['wavelength'], 0.1) == 2.5
-    assert pytest.approx(dd.beam.wavelength, 0.1) == 2.5
-    print(dd.beam)
-
-
-def test_write_EMC_ini(tmp_path):
-    h5_file = './testFiles/singfel-multi.h5'
-    out_path = tmp_path / "config.ini"
-    # out_path = 'config.ini'
-    dd = DD.read(h5_file)
-    dd.writeEmcIni(str(out_path))
-
-
-if __name__ == "__main__":
-    # pass
-    # test_writeUnsupported()
-    # test_print_format_keys()
-    # test_write_emc_h5_multi(Path('./'))
-    # test_read_emc_h5_multi()
-    # plt.show()
-    test_data_simplebeam()
-    # test_filetype_content()
-    # test_getDataType()
-    # test_addBeamStop()
-    # test_photon_statistics()
-    # test_plotPattern()
-    # test_plotStatistics()
-    # test_multiply()
-    # test_iterator()
+    h5_file = "./testFiles/singfel-multi.h5"
+    DiffrData = DiffractionData.from_file(
+        h5_file, format_class=SingfelFormat, key="test_singfel", poissonize=False
+    )
+    ofn = str(tmp_path / "rewrite_singfel.h5")
+    DiffrData.write(
+        ofn,
+        SingfelFormat,
+    )
