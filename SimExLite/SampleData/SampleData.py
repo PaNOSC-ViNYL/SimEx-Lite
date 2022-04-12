e830a3c52d48fbf06faf07643bb216db7b7d4703
Juncheng E <juncheng.e@xfel.eu>
2022-04-12 17:52:29 +0200

    test_SampleData: add missed asserts

--- SimExLite/SampleData/SampleData.py
+++ SimExLite/SampleData/SampleData.py
@@ -2,7 +2,7 @@
 # Contact: Juncheng E <juncheng.e@xfel.eu>
 # This file is part of SimEx-Lite which is released under GNU General Public License v3.
 # See file LICENSE or go to <http://www.gnu.org/licenses> for full license details.
-"""Wavefront Data APIs"""
+"""Sample Data APIs"""
 
 from libpyvinyl import BaseData
 
