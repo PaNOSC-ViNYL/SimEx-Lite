f37590a13dabe53f6acb86dff6bfd16259a48235
Juncheng E <juncheng.e@xfel.eu>
2023-06-29 14:33:49 +0200

    add DetectorData

---
+++ SimExLite/DetectorData/__init__.py
@@ -0,0 +1,8 @@
+# Copyright (C)  Juncheng E
+# Contact: Juncheng E <juncheng.e@xfel.eu>
+# This file is part of SimEx-Lite which is released under GNU General Public License v3.
+# See file LICENSE or go to <http://www.gnu.org/licenses> for full license details.
+"""DetectorData module for SimEx-Lite."""
+
+from .DetectorData import DetectorData
+from .CXIFormat import CXIFormat
