492af3e846d2a29b72d6983466e365a308ffb6bd
Juncheng E <juncheng.e@xfel.eu>
2023-07-04 17:07:44 +0200

    Fix PMIData test

--- tests/test_PMIData.py
+++ tests/test_PMIData.py
@@ -6,7 +6,7 @@ from SimExLite.PMIData import PMIData, XMDYNFormat
 
 def test_create_from_file():
     """Test creating a data from a file"""
-    testfile = "./testFiles/pmi_out_0000001.h5"
+    testfile = "./testFiles/PMI.h5"
     PD = PMIData.from_file(testfile, XMDYNFormat, key="XMDYN_data")
     data_dict = PD.get_data()
     assert "angle", "0" in data_dict.keys()
