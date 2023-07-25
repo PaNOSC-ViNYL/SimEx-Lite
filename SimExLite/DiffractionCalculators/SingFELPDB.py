5d0039e1cd14b1be2279fb15346d3c6aef58cba0
Juncheng E <juncheng.e@xfel.eu>
2023-07-25 11:16:19 +0200

    Working PDB calculator

---
+++ SimExLite/DiffractionCalculators/SingFELPDB.py
@@ -0,0 +1,167 @@
+#!/usr/bin/env python3
+
+import argparse
+import copy
+import os
+import sys
+
+from pysingfel.FileIO import saveAsDiffrOutFile, prepH5
+from pysingfel.beam import Beam
+from pysingfel.detector import Detector
+from pysingfel.diffraction import calculate_molecularFormFactorSq
+from pysingfel.particle import Particle
+from pysingfel.radiationDamage import generateRotations, rotateParticle
+from pysingfel.toolbox import convert_to_poisson
+
+
+def parse_input(args):
+    """
+    Parse the input command arguments and return a dict containing all simulation parameters.
+    """
+
+    def ParseBoolean(b):
+        # Handle different possible Boolean types.
+        if b is None or b == "None":
+            return b
+        if b == False or b == True:
+            return b
+        b = b.strip()
+        if len(b) < 1:
+            raise ValueError("Cannot parse empty string into boolean.")
+        b = b[0].lower()
+        if b == "t" or b == "y" or b == "1":
+            return True
+        if b == "f" or b == "n" or b == "0":
+            return False
+        raise ValueError("Cannot parse string into boolean.")
+
+    # fmt: off
+    # Instantiate the parser
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--inputFile', help='Input PDF file')
+    parser.add_argument('--outputDir', help='Output directory for saving diffraction')
+    parser.add_argument('--beamFile', help='Beam file defining X-ray beam')
+    parser.add_argument('--geomFile', help='Geometry file defining diffraction geometry')
+    parser.add_argument('--rotationAxis', default='xyz', help='Euler rotation convention')
+    parser.add_argument('--orientation',nargs='+', type=float, help='Specify the quternion r i j k of '
+                        'each diffraction pattern. It overrides --unifromRotation')
+    parser.add_argument('--uniformRotation', type=ParseBoolean,
+                        help='If 1, rotates the sample uniformly in SO(3),\
+                                if 0 random orientation in SO(3),\
+                                if None (omitted): no orientation.')
+    parser.add_argument('--numDP', type=int, help='Number of diffraction patterns per PMI file')
+    # fmt: on
+
+    # convert argparse to dict
+    return vars(parser.parse_args(args))
+
+
+def main(parameters):
+    from mpi4py import MPI
+
+    # Initialize MPI
+    mpi_comm = MPI.COMM_WORLD
+    mpi_rank = mpi_comm.Get_rank()
+    mpi_size = mpi_comm.Get_size()
+
+    # parameters
+    numDP = int(parameters["numDP"])
+    input_fn = parameters["inputFile"]
+    output_dir = parameters["outputDir"]
+    uniform_rotation = parameters["uniformRotation"]
+    beamFile = parameters["beamFile"]
+    geomFile = parameters["geomFile"]
+    orientation = parameters["orientation"]
+
+    # Perform common work on all cores.
+    initial_particle = Particle()
+    initial_particle.readPDB(input_fn, ff="WK")
+
+    # Generate rotations.
+    quaternions = generateRotations(uniform_rotation, "xyz", numDP, orientation)
+
+    detector = Detector(geomFile)
+    beam = Beam(beamFile)
+    detector.init_dp(beam)
+
+    # Determine which patterns to run on which core.
+    number_of_patterns_per_core = numDP // mpi_size
+    # Remainder of the division.
+    remainder = numDP % mpi_size
+    # Pattern indices
+    pattern_indices = list(range(numDP))
+
+    # Distribute patterns over cores.
+    rank_indices = pattern_indices[
+        mpi_rank
+        * number_of_patterns_per_core : (mpi_rank + 1)
+        * number_of_patterns_per_core
+    ]
+    # Distribute remainder
+    if mpi_rank < remainder:
+        rank_indices.append(
+            pattern_indices[mpi_size * number_of_patterns_per_core + mpi_rank]
+        )
+        # Setup the output file.
+    outputName = (
+        output_dir
+        + "/diffr_out_"
+        + "{0:07}".format(mpi_comm.Get_rank() + 1)
+        + ".h5"
+    )
+
+    if os.path.exists(outputName):
+        os.remove(outputName)
+
+    output_is_ready = False
+    # Loop over assigned tasks
+    for pattern_index in rank_indices:
+        # Setup the output hdf5 file if not already done.
+        if not output_is_ready:
+            prepH5(outputName)
+            output_is_ready = True
+
+        # Make local copy of the sample.
+        particle = copy.deepcopy(initial_particle)
+
+        # Rotate particle.
+        quaternion = quaternions[pattern_index, :]
+        rotateParticle(quaternion, particle)
+
+        # Calculate the diffraction intensity.
+        detector_intensity = calculate_molecularFormFactorSq(particle, detector)
+
+        # Correct for solid angle
+        detector_intensity *= detector.solidAngle
+
+        # Correct for polarization
+        detector_intensity *= detector.PolarCorr
+
+        # Multiply by photon fluence.
+        detector_intensity *= beam.get_photonsPerPulsePerArea()
+
+        # Poissonize.
+        detector_counts = convert_to_poisson(detector_intensity)
+
+        # Save to h5 file.
+        saveAsDiffrOutFile(
+            outputName,
+            None,
+            pattern_index,
+            detector_counts,
+            detector_intensity,
+            quaternion,
+            detector,
+            beam,
+        )
+
+        del particle
+
+    mpi_comm.Barrier()
+
+    return 0
+
+
+if __name__ == "__main__":
+    parameters = parse_input(sys.argv[1:])
+    main(parameters=parameters)
---
+++ SimExLite/DiffractionCalculators/SingFELPDBDiffractionCalculator.py
@@ -0,0 +1,256 @@
+
+# Copyright (C) 2023 Juncheng E
+# Contact: Juncheng E <juncheng.e@xfel.eu>
+# This file is part of SimEx-Lite which is released under GNU General Public License v3.
+# See file LICENSE or go to <http://www.gnu.org/licenses> for full license details.
+
+import os
+from pathlib import Path
+from subprocess import Popen, PIPE
+import shlex
+import h5py
+import numpy as np
+from libpyvinyl.BaseCalculator import BaseCalculator, CalculatorParameters
+from libpyvinyl.BaseData import DataCollection
+from SimExLite.DiffractionData import DiffractionData, SingFELFormat
+from SimExLite.PhotonBeamData import SimpleBeam
+from SimExLite.utils.Logger import setLogger
+
+logger = setLogger("SingFELPDBDiffractionCalculator")
+
+
+class SingFELPDBDiffractionCalculator(BaseCalculator):
+    """SingFEL diffraction pattern from pdb file calculator."""
+
+    def __init__(
+        self,
+        name: str,
+        input: DataCollection,
+        sample: str,
+        output_keys: str = "singfelPDB_diffraction",
+        output_data_types=DiffractionData,
+        output_filenames: str = "diffr.h5",
+        instrument_base_dir="./",
+        calculator_base_dir="SingFELPDBDiffractionCalculator",
+        parameters=None,
+    ):
+        super().__init__(
+            name,
+            input,
+            output_keys,
+            output_data_types=output_data_types,
+            output_filenames=output_filenames,
+            instrument_base_dir=instrument_base_dir,
+            calculator_base_dir=calculator_base_dir,
+            parameters=parameters,
+        )
+        self.sample = sample
+
+    def init_parameters(self):
+        parameters = CalculatorParameters()
+        random_rotation = parameters.new_parameter(
+            "random_rotation",
+            comment="If it's False, the orientations are fixed to ensure the SO3 space is always uniformly sampled."
+            + " If it's True, it will be a random sampling complying a uniform distribution in the SO3 space",
+        )
+        random_rotation.value = True
+
+        number_of_diffraction_patterns = parameters.new_parameter(
+            "number_of_diffraction_patterns",
+            comment="The number of diffraction patterns to generate",
+        )
+        pixel_size = parameters.new_parameter(
+            "pixel_size", comment="The pixel size of the detector", unit="m"
+        )
+        pixels_x = parameters.new_parameter(
+            "pixels_x", comment="Number of pixels in x direction"
+        )
+        pixels_y = parameters.new_parameter(
+            "pixels_y", comment="Number of pixels in y direction"
+        )
+        distance = parameters.new_parameter(
+            "distance", comment="Sample to detector distance", unit="m"
+        )
+        mpi_command = parameters.new_parameter(
+            "mpi_command", comment="The mpi command to run pysingfel"
+        )
+
+        number_of_diffraction_patterns.value = 10
+        pixel_size.value = 0.001
+        pixels_x.value = 10
+        pixels_y.value = 5
+        distance.value = 0.13
+        mpi_command.value = "mpirun -n 2"
+
+        self.parameters = parameters
+
+    def backengine(self):
+        self.parse_input()
+        exec_bin = Path(__file__).parent / "SingFELPDB.py"
+        output_stem = str(Path(self.output_file_paths[0]).stem)
+        output_dir = Path(self.output_file_paths[0]).parent / output_stem
+        geom_file = self.get_geometry_file()
+        beam_file = self.get_beam_file()
+        uniform_rotation = not self.parameters["random_rotation"].value
+        number_of_diffraction_patterns = self.parameters[
+            "number_of_diffraction_patterns"
+        ].value
+        mpi_command = self.parameters["mpi_command"].value
+        # fmt: off
+        output_dir.mkdir(parents=True, exist_ok=False)
+        # TODO: include single orientation
+        command_sequence = ['python3',             str(exec_bin),
+                            '--inputFile',         str(self.sample),
+                            '--outputDir',        str(output_dir),
+                            '--geomFile',         str(geom_file),
+                            '--beamFile',         str(beam_file),
+                            '--uniformRotation',  str(uniform_rotation),
+                            '--numDP',            str(number_of_diffraction_patterns),
+                            ]
+        # fmt: on
+        args = shlex.split(mpi_command) + command_sequence
+        proc = Popen(args, stdin=PIPE, stdout=PIPE, stderr=PIPE)
+        # proc.wait()
+        # The above one can be replaced by proc.communicate()
+        output, err = proc.communicate()
+        rc = proc.returncode
+        if rc != 0:
+            print(output.decode("ascii"))
+            raise RuntimeError(err.decode("ascii"))
+        saveH5(str(output_dir))
+        assert len(self.output_keys) == 1
+        key = self.output_keys[0]
+        output_data = self.output[key]
+        output_data.set_file(self.output_file_paths[0], SingFELFormat)
+        return self.output
+
+    def parse_input(self):
+        """Check the beam data"""
+        assert len(self.input) == 1
+        # self.sample_fn = self.input.to_list()[0]
+        self.photon_beam = self.input.to_list()[0]
+        assert isinstance(self.photon_beam, SimpleBeam)
+        
+    def get_geometry_file(self):
+        simple_config = {
+            "pixel_size": self.parameters["pixel_size"].value,
+            "slow_pixels": self.parameters["pixels_y"].value,
+            "fast_pixels": self.parameters["pixels_x"].value,
+        }
+        distance = self.parameters["distance"].value
+        filename = str(Path(self.base_dir) / "singfel.geom")
+        write_singfel_geom_file(filename, simple_config, distance)
+        return filename
+
+    def get_beam_file(self):
+        filename = str(Path(self.base_dir) / "singfel.beam")
+        photon_energy = self.photon_beam.get_photon_energy().magnitude
+        n_photons = self.photon_beam.get_photons_per_pulse().magnitude
+        beam_size = self.photon_beam.get_beam_size().magnitude
+        # To get the same area
+        diameter = np.sqrt(beam_size[0]*beam_size[1])
+        write_singfel_beam_file(filename, photon_energy, n_photons, diameter)
+        return filename
+
+
+def write_singfel_beam_file(file_name, photon_energy, n_photons, diameter):
+    """Write the beam file for pysingfel
+
+    :param file_name: The file name of the beam file.
+    :type output_path: string
+    :param photon_energy: The photon energy in eV.
+    :type photon_energy: float
+    :param n_photons: The number of photons per pulse.
+    :type n_photons: float
+    :param diameter: The diameter of a round beam spot.
+    :type diameter: float
+    """
+    strings = f"beam/photon_energy = {photon_energy}\n"
+    strings += f"beam/fluence = {n_photons}\n"
+    # This 'radius' is treated as diameter in pysingfel'
+    strings += f"beam/radius = {diameter}\n"
+    with open(file_name, "w") as fh:
+        fh.write(strings)
+
+
+def write_singfel_geom_file(file_name, simeple_config, distance):
+    """Write the geom file for pysingfel"""
+    # fmt: off
+    panel_id_str = 0
+    serialization = ";panel %s\n" % (panel_id_str)
+    serialization += "panel%s/min_fs         = %d\n" % (panel_id_str, 0)
+    serialization += "panel%s/max_fs         = %d\n" % (panel_id_str, simeple_config["fast_pixels"]-1)
+    serialization += "panel%s/min_ss         = %d\n" % (panel_id_str, 0)
+    serialization += "panel%s/max_ss         = %d\n" % (panel_id_str, simeple_config["slow_pixels"]-1)
+    # fmt: on
+    # This is allowed to be float
+    corner_x = -(simeple_config["fast_pixels"] - 1) / 2
+    corner_y = -(simeple_config["slow_pixels"] - 1) / 2
+    # fmt: off
+    serialization += "panel%s/fs             = %s\n" % (panel_id_str, "1.0x")
+    serialization += "panel%s/ss             = %s\n" % (panel_id_str, "1.0y")
+    serialization += "panel%s/clen           = %8.7e\n" % (panel_id_str, distance)
+    serialization += "panel%s/res            = %8.7e\n" % (panel_id_str, 1 / simeple_config["pixel_size"])
+    serialization += "panel%s/coffset        = %8.7e\n" % (panel_id_str, 0)
+    serialization += "panel%s/px             = %d\n" % (panel_id_str, simeple_config["fast_pixels"])
+    serialization += "panel%s/py             = %d\n" % (panel_id_str, simeple_config["slow_pixels"])
+    serialization += "panel%s/pix_width      = %8.7e\n" % (panel_id_str, simeple_config["pixel_size"])
+    serialization += "panel%s/d              = %8.7e\n" % (panel_id_str, distance)
+    serialization += "panel%s/corner_x       = %d\n" % (panel_id_str, corner_x)
+    serialization += "panel%s/corner_y       = %d\n" % (panel_id_str, corner_y)
+    serialization += "\n"
+    # fmt: on
+
+    with open(file_name, "w") as fh:
+        fh.write(serialization)
+
+
+def saveH5(path_to_files):
+    """
+    Private method to save the object to a file. Creates links to h5 files that all contain only one pattern.
+
+    :param output_path: The file where to save the object's data.
+    :type output_path: string, default b
+    """
+
+    # Setup new file.
+    with h5py.File(path_to_files + ".h5", "w") as h5_outfile:
+
+        # Files to read from.
+        individual_files = [
+            os.path.join(path_to_files, f) for f in os.listdir(path_to_files)
+        ]
+        individual_files.sort()
+
+        # Keep track of global parameters being linked.
+        global_parameters = False
+        # Loop over all individual files and link in the top level groups.
+        for ind_file in individual_files:
+            # Open file.
+            with h5py.File(ind_file, "r") as h5_infile:
+
+                # Links must be relative.
+                relative_link_target = os.path.relpath(
+                    path=ind_file, start=os.path.dirname(os.path.dirname(ind_file))
+                )
+
+                # Link global parameters.
+                if not global_parameters:
+                    global_parameters = True
+
+                    h5_outfile["params"] = h5py.ExternalLink(
+                        relative_link_target, "params"
+                    )
+                    h5_outfile["info"] = h5py.ExternalLink(relative_link_target, "info")
+                    h5_outfile["misc"] = h5py.ExternalLink(relative_link_target, "misc")
+                    h5_outfile["version"] = h5py.ExternalLink(
+                        relative_link_target, "version"
+                    )
+
+                for key in h5_infile["data"]:
+
+                    # Link in the data.
+                    ds_path = "data/%s" % (key)
+                    h5_outfile[ds_path] = h5py.ExternalLink(
+                        relative_link_target, ds_path
+                    )
