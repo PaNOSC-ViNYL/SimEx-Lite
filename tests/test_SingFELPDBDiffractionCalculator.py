7cf73b7c5cc5aaff2de6d2966c34c466b91012e9
Juncheng E <juncheng.e@xfel.eu>
2023-08-16 14:20:02 +0200

    SingFELPDBDiffractionCalculator: reduce the number of pixels to test

--- tests/test_SingFELPDBDiffractionCalculator.py
+++ tests/test_SingFELPDBDiffractionCalculator.py
@@ -7,6 +7,7 @@ pytestmark = pytest.mark.skipif(
 )
 from SimExLite.DiffractionCalculators import SingFELPDBDiffractionCalculator
 from SimExLite.PhotonBeamData import SimpleBeam
+from SimExLite.SampleData import SampleData, ASEFormat
 from SimExLite import DataCollection
 
 
@@ -15,10 +16,10 @@ def test_construct_calculator(tmpdir):
     beam_data = SimpleBeam(
         photons_per_pulse=4e12, photon_energy=6000, beam_size=[228e-9, 228e-9]
     )
+    sample_data = SampleData.from_file("testFiles/2nip.pdb", ASEFormat, "sample")
     diffraction = SingFELPDBDiffractionCalculator(
         name="SingFELPDBDiffractionCalculator",
-        input=DataCollection(beam_data),
-        sample="testFiles/2nip.pdb",
+        input=DataCollection(beam_data, sample_data),
         instrument_base_dir=str(tmpdir),
     )
     print(diffraction.parameters)
@@ -29,14 +30,14 @@ def test_run(tmpdir):
     beam_data = SimpleBeam(
         photons_per_pulse=4e12, photon_energy=6000, beam_size=[228e-9, 228e-9]
     )
+    sample_data = SampleData.from_file("testFiles/2nip.pdb", ASEFormat, "sample")
     diffraction = SingFELPDBDiffractionCalculator(
         name="SingFELPDBDiffractionCalculator",
-        input=DataCollection(beam_data),
-        sample="testFiles/2nip.pdb",
+        input=DataCollection(beam_data, sample_data),
         instrument_base_dir=str(tmpdir),
     )
-    diffraction.parameters["pixels_x"] = 1092
-    diffraction.parameters["pixels_y"] = 1256
+    diffraction.parameters["pixels_x"] = 10
+    diffraction.parameters["pixels_y"] = 5
     diffraction.parameters["pixel_size"] = 200e-6
     print(diffraction.parameters)
     output = diffraction.backengine()
@@ -48,15 +49,15 @@ def test_run_bin(tmpdir):
     beam_data = SimpleBeam(
         photons_per_pulse=4e12, photon_energy=6000, beam_size=[228e-9, 228e-9]
     )
+    sample_data = SampleData.from_file("testFiles/2nip.pdb", ASEFormat, "sample")
     diffraction = SingFELPDBDiffractionCalculator(
         name="SingFELPDBDiffractionCalculator",
-        input=DataCollection(beam_data),
-        sample="testFiles/2nip.pdb",
+        input=DataCollection(beam_data, sample_data),
         instrument_base_dir=str(tmpdir),
     )
     diffraction.parameters["number_of_diffraction_patterns"] = 2
-    diffraction.parameters["pixels_x"] = 546
-    diffraction.parameters["pixels_y"] = 628
+    diffraction.parameters["pixels_x"] = 10
+    diffraction.parameters["pixels_y"] = 5
     diffraction.parameters["pixel_size"] = 400e-6
     print(diffraction.parameters)
     output = diffraction.backengine()
